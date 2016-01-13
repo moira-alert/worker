f102b53fa6ff45bd3126aa1ca7ccc9877f04dffc
Alexey Larkov <gmlexx@gmail.com>
2016-01-13 18:44:24 +0500

    Add scripts entry_points

--- moira/api/server.py
+++ moira/api/server.py
@@ -1,27 +1,38 @@
 import sys
-import os
-
-sys.path.append(
-    os.path.abspath(
-        os.path.join(
-            os.path.abspath(
-                os.path.dirname(__file__)),
-            '../../')))
-
-from moira import config
 from twisted.application import service, internet
+from twisted.internet import reactor
+from twisted.python import log
+from moira import config
 from moira.graphite import datalib
 from moira.db import Db
 from moira.api.site import Site
+from moira import logs
 
-topService = service.MultiService()
 
-db = Db()
-datalib.db = db
-db.setServiceParent(topService)
+def run():
 
-httpService = internet.TCPServer(config.HTTP_PORT, Site(db), interface=config.HTTP_ADDR)
-httpService.setServiceParent(topService)
+    parser = config.get_parser()
+    parser.add_argument('-port', help='listening port', default=8081, type=int)
+    args = parser.parse_args()
 
-application = service.Application("moira-worker-api")
-topService.setServiceParent(application)
+    config.CONFIG_PATH = args.c
+    config.HTTP_PORT = args.port
+    config.LOG_DIRECTORY = args.l
+
+    logger = logs.api() if args.l != "stdout" else sys.stdout
+    log.startLogging(logger)
+
+    topService = service.MultiService()
+
+    db = Db()
+    datalib.db = db
+    db.setServiceParent(topService)
+
+    httpService = internet.TCPServer(config.HTTP_PORT, Site(db), interface=config.HTTP_ADDR)
+    httpService.setServiceParent(topService)
+
+    topService.startService()
+
+    reactor.addSystemEventTrigger('before', 'shutdown', topService.stopService)
+
+    reactor.run()
--- moira/checker/server.py
+++ moira/checker/server.py
@@ -1,4 +1,6 @@
 import multiprocessing
+import os
+import sys
 from sys import executable
 from os import environ
 from twisted.application import service
@@ -8,8 +10,8 @@ from twisted.internet.protocol import ProcessProtocol
 from moira.checker.subscriber import SubscriberService
 from moira.graphite import datalib
 from moira.db import Db
-import os
-
+from moira import config
+from moira import logs
 
 CHECKER_PATH = os.path.abspath(
     os.path.join(
@@ -36,23 +38,34 @@ class TopService(service.MultiService):
         service.MultiService.startService(self)
         for i in range(max(1, multiprocessing.cpu_count() - 1)):
             checker = reactor.spawnProcess(
-                CheckerProcessProtocol(), executable, [
-                    'moira-checker', CHECKER_PATH, str(i)], childFDs={
-                    0: 'w', 1: 1, 2: 2}, env=environ)
+                CheckerProcessProtocol(), executable,
+                ['moira-checker', CHECKER_PATH, "-n", str(i), "-c", config.CONFIG_PATH, "-l", config.LOG_DIRECTORY],
+                childFDs={0: 'w', 1: 1, 2: 2}, env=environ)
             self.checkers.append(checker)
 
-    def stopService(self):
-        for process in self.checkers:
-            process.signalProcess('INT')
 
-topService = TopService()
+def run():
 
-db = Db()
-datalib.db = db
-db.setServiceParent(topService)
+    parser = config.get_parser()
+    args = parser.parse_args()
 
-subService = SubscriberService(db)
-subService.setServiceParent(topService)
+    config.CONFIG_PATH = args.c
+    config.LOG_DIRECTORY = args.l
 
-application = service.Application("moira-worker-subscriber")
-topService.setServiceParent(application)
+    logger = logs.checker() if args.l != "stdout" else sys.stdout
+    log.startLogging(logger)
+
+    topService = TopService()
+
+    db = Db()
+    datalib.db = db
+    db.setServiceParent(topService)
+
+    subService = SubscriberService(db)
+    subService.setServiceParent(topService)
+
+    topService.startService()
+
+    reactor.addSystemEventTrigger('before', 'shutdown', topService.stopService)
+
+    reactor.run()
