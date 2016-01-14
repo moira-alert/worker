519f86126dd681032204ee56d814a1c5af329d32
Alexey Larkov <gmlexx@gmail.com>
2016-01-14 19:48:16 +0500

    Update version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "moira",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "SKB Kontur",
     author_email = "devops@skbkontur.ru",
     description = "Moira checker and api modules",
