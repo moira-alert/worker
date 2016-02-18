542623b3e8a3ee5ff9b4107ed0615e8fc5fdb223
Alexey Larkov <gmlexx@gmail.com>
2016-02-18 16:48:18 +0500

    Add sleep interval between checks to more linear workers load

--- moira/checker/worker.py
+++ moira/checker/worker.py
@@ -39,7 +39,8 @@ class TriggersCheck:
                 end = reactor.seconds()
                 spy.TRIGGER_CHECK.report(end - start)
                 trigger_id = yield self.db.getTriggerToCheck()
-            yield task.deferLater(reactor, random.uniform(PERFORM_INTERVAL * 10, PERFORM_INTERVAL * 20), lambda: None)
+                yield task.deferLater(reactor, PERFORM_INTERVAL, lambda: None)
+            yield task.deferLater(reactor, PERFORM_INTERVAL * 2, lambda: None)
         except GeneratorExit:
             pass
         except:
