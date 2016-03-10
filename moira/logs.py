b1512ae7772b856bc989a2f95b818e8819ade337
Alexey Larkov <gmlexx@gmail.com>
2016-03-10 16:50:43 +0500

    Make zero padding log

--- moira/logs.py
+++ moira/logs.py
@@ -6,6 +6,17 @@ from twisted.python.logfile import DailyLogFile
 from moira import config
 
 
+class ZeroPaddingDailyLogFile(DailyLogFile):
+
+    def suffix(self, tupledate):
+        """Return the suffix given a (year, month, day) tuple or unixtime"""
+        try:
+            return ''.join(map(lambda x: ("0%d" % x) if x < 10 else str(x), tupledate))
+        except:
+            # try taking a float unixtime
+            return ''.join(map(str, self.toDate(tupledate)))
+
+
 def api():
     if config.LOG_DIRECTORY == "stdout":
         log.startLogging(sys.stdout)
@@ -31,4 +42,4 @@ def daily(name):
     path = os.path.abspath(config.LOG_DIRECTORY)
     if not os.path.exists(path):
         os.makedirs(path)
-    return DailyLogFile(name, path)
+    return ZeroPaddingDailyLogFile(name, path)
