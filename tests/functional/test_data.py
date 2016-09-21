102aff445bb416f3b15d7ee9ef872b97e3ad0a6f
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2016-09-21 19:45:38 +0500

    fix tests

--- tests/functional/test_data.py
+++ tests/functional/test_data.py
@@ -230,9 +230,8 @@ class DataTests(WorkerTests):
         yield self.db.sendMetric(pattern, metric, begin, 10)
         yield self.db.sendMetric(pattern, metric, begin + 60, 20)
         yield self.db.sendMetric(pattern, metric, begin + 120, 30)
-        yield self.trigger.check(fromTime=begin, now=begin + 120)
-        yield self.assert_trigger_metric('summarize(' + metric +
-                                         ', "10min", "sum")', 60, state.ERROR)
+        yield self.trigger.check(fromTime=begin, now=begin + 180)
+        yield self.assert_trigger_metric('summarize(' + metric + ', "10min", "sum")', 60, state.ERROR)
 
     @trigger('test-trigger-summarize-min')
     @inlineCallbacks
@@ -671,10 +670,10 @@ class DataTests(WorkerTests):
         yield self.sendTrigger('{"name": "test trigger", "targets": [" \
                                maximumAbove(metric.*, 0)"], "warn_value": 60, "error_value": 90}')
         yield self.db.sendMetric("metric.one", "metric.one", self.now, 1)
-        yield self.trigger.check(now=self.now, cache_ttl=0)
+        yield self.trigger.check(now=self.now + 60, cache_ttl=0)
         yield self.db.cleanupMetricValues("metric.one", self.now + 3600)
         yield self.db.sendMetric("metric.two", "metric.two", self.now + 60, 1)
-        yield self.trigger.check(now=self.now + 60, cache_ttl=0)
+        yield self.trigger.check(now=self.now + 120, cache_ttl=0)
         yield self.assert_trigger_metric("metric.one", 1, state.OK)
 
     @trigger('test-schedule2')
